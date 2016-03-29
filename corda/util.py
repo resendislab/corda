9f5f696ebb3fcb1f219da12849549435d9b95aaa
Christian Diener <ch.diener@gmail.com>
2016-03-29 10:55:02 -0600

    Eval of gene-reaction rules is now safe.
    Gene-reaction rules are now evaluated by an AST rather than calling
    eval directly. Thanks to cobrapy for the nice reference implementation.

--- corda/util.py
+++ corda/util.py
@@ -1,51 +1,59 @@
 #  util.py
-#  
+#
 #  Copyright 2016 Christian Diener <mail[at]cdiener.com>
-#  
+#
 #  MIT license. See LICENSE for more information.
 
 import re
 from cobra.manipulation import revert_to_reversible
+from cobra.core.Gene import parse_gpr
+from ast import Name, And, Or, BoolOp, Expression
 
-class ci(int): 
+class ci(int):
     def __and__(self, other):
         return min(self, other)
-    
+
     def __or__(self, other):
         return max(self, other)
 
 def format_gid(gid):
     return re.sub(r"\.\d*", "", gid)
 
+def safe_eval_gpr(expr, conf_genes):
+    """safely evaluate confidence of a gene-reaction rule"""
+    if isinstance(expr, Expression):
+        return safe_eval_gpr(expr.body, conf_genes)
+    elif isinstance(expr, Name):
+        if expr.id not in conf_genes: return 0
+        return conf_genes[expr.id]
+    elif isinstance(expr, BoolOp):
+        op = expr.op
+        if isinstance(op, Or):
+            return max(safe_eval_gpr(i, conf_genes) for i in expr.values)
+        elif isinstance(op, And):
+            return min(safe_eval_gpr(i, conf_genes) for i in expr.values)
+        else:
+            raise TypeError("unsupported operation " + op.__class__.__name__)
+    elif expr is None:
+        return 0
+    else:
+        raise TypeError("unsupported operation  " + repr(expr))
+
 def reaction_confidence(rule, conf_genes):
     """Calculates the confidence for the reaction based on a gene-reaction
         rule"""
-    
-    if rule == '': return 0
-    
-    rule = rule.replace("and", "&").replace("or", "|")
-    matches = re.finditer(r"\w*\d+\.*\d*", rule)
-    shift = 0
-    for m in matches:
-        fgid = format_gid(m.group())
-        rep = "ci(" + str(conf_genes[fgid]) + ")" if fgid in conf_genes \
-            else "ci(0)"
-        # replace won't work here because IDs may include other IDs 
-        # for instance 1234.1 includes 34.1 and 234.1 etc.
-        s, e = m.start() + shift, m.end() + shift
-        rule = rule[:s] + rep + rule[e:]
-        shift += len(rep) - len(m.group())
-    
-    return eval(rule)
-    
+
+    ast_rule, _ = parse_gpr(rule)
+    return safe_eval_gpr(ast_rule, conf_genes)
+
 
 def safe_revert_reversible(model):
     maybe_rev = (r for r in model.reactions if "reflection" in r.notes)
-    
+
     for r in maybe_rev:
         try:
             model.reactions.get_by_id(r.notes["reflection"])
         except KeyError:
             r.notes.pop("reflection")
-    
+
     revert_to_reversible(model)
