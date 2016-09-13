2d891315a86b5a6f303d95b2d2961306eb4e2777
Christian Diener <ch.diener@gmail.com>
2016-09-13 13:23:40 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='corda',
-    version='0.1.2',
+    version='0.1.3',
     description='Genome-scale model construction with CORDA',
     long_description=long_description,
     url='https://github.com/cdiener/corda',
